b5e154c43416825672a83f91d412d7de331986a8
Naunter <17890335+Naunter@users.noreply.github.com>
2021-03-19 04:46:56 -0400

    Update run.py

--- src/py/run.py
+++ src/py/run.py
@@ -46,7 +46,7 @@ else:
     showinfo('提示', '请先运行黑色沙漠的启动器并等待其更新完毕后, 再执行汉化任务!')
 
 check = check_client_version.get_version()
-if check != ('2021031404' and False):
+if check != '2021031404' or check is False:
     print("run.py >>> check_client_version.get_version() is True")
     a = askyesno('提示', '有新版本的客户端，是否查看？')
     if a == True:
